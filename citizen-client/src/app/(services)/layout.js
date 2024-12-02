01863e696619b2f5674436937841850b469aa5b0
Mohammad Al Amin <alaminbamna08@gmail.com>
2024-12-03 02:48:58 +0600

    servie page some update

--- citizen-client/src/app/(services)/layout.js
+++ citizen-client/src/app/(services)/layout.js
@@ -6,11 +6,11 @@ export default function ServiceLayout({ children }) {
   return (
     <div className="custom-container mt-[10px]  ">
       <div className="grid grid-cols-12">
-        <div className="md:col-span-4 xs:col-span-12">
+        <div className="md:col-span-4 sm:col-span-12">
           <ServiceSidebar />
         </div>
 
-        <main className="md:col-span-8 xs:col-span-12 md:mx-0 xs:mx-3">
+        <main className="md:col-span-8 sm:col-span-12 md:mx-0 sm:mx-3">
           {children}
         </main>
       </div>
