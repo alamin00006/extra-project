877504d7d0258bbc11b4b96ddc8e300bb9be4297
Mohammad Al Amin <alaminbamna08@gmail.com>
2025-01-30 02:10:52 +0600

    server url update

--- citizen-client/src/helpers/config/envConfig.js
+++ citizen-client/src/helpers/config/envConfig.js
@@ -1,10 +1,10 @@
-// export const getBaseUrl = () => {
-//   return "https://api.citizencarebd.com/api/v1";
-// };
+export const getBaseUrl = () => {
+  return "https://api.citizencarebd.com/api/v1";
+};
 // export const getMainBaseUrl = () => {
 //   // return "http://localhost:5000";
 // };
 
-export const getBaseUrl = () => {
-  return "http://localhost:5000/api/v1";
-};
+// export const getBaseUrl = () => {
+//   return "http://localhost:5000/api/v1";
+// };
