c31048c52e91f6c1e81de17d1a81e36361c7634e
Mohammad Al Amin <alaminbamna08@gmail.com>
2025-05-29 05:02:56 +0600

    some update

--- citizen-client/src/components/Home/Banner2.jsx
+++ citizen-client/src/components/Home/Banner2.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const images = [
-  "/images/home-page/Health-Care1.jpg",
+  "/images/home-page/Health-Care.jpg",
   "/images/home-page/Home-Page-Front.jpg",
   "/images/home-page/CCB-CUMMUNITY.jpg",
   "/images/home-page/Awerness-Program.jpg",
